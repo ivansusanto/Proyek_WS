27042627d18f6322e2ef45e546337aea0052c4fb
equerlyn <feliciapangestu.fp@gmail.com>
2023-05-11 12:38:39 +0700

    menambah tipe data

--- src/controllers/developersController.ts
+++ src/controllers/developersController.ts
@@ -7,39 +7,48 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const prisma = new PrismaClient();
-const saltRounds = 10;
-const salt = bcrypt.genSaltSync(saltRounds);
+const saltRounds:number = 10;
+const salt: string | number = bcrypt.genSaltSync(saltRounds);
 const SECRET_KEY: string = process.env.SECRET_KEY?.toString() ?? "";
 
 export async function registerDeveloper(req : Request, res : Response) {
-    const { username, password, email, full_name, display_name } = req.body;
-    const dev_id = await generateDeveloperId();
-    const api_key_dev = await generateApiKey();
+    const { username, password, email, full_name, display_name }: {
+      username: string,
+      password: string,
+      email: string,
+      full_name: string,
+      display_name: string
+    } = req.body;
+    const dev_id : string = await generateDeveloperId();
+    const api_key_dev : string = await generateApiKey();
     const token = jwt.sign({ api_key: api_key_dev }, SECRET_KEY, { expiresIn: '1h' });
-    
+    const hashedPassword: string = bcrypt.hashSync(password, salt);
     await prisma.developers.create({
         data: {
             developer_id: dev_id,
             username: username,
-            password: bcrypt.hashSync(password, salt),
+            password:hashedPassword,
             email: email,
             full_name: full_name,
             display_name: display_name,
             api_key: api_key_dev
         }
     });
-
     return res.status(201).send({ token: token });
 }
 
 export async function loginDeveloper(req : Request, res : Response) {
-    
+    const {email, username, password}:{
+      username: string,
+      password: string,
+      email: string,
+    } = req.body
+
 }
 
 async function generateDeveloperId(): Promise<string> {
     const developerCount = await prisma.developers.count();
     const newIdNumber = (developerCount + 1).toString();
-
     const paddingSize = 4 - newIdNumber.length;
     const zeroPadding = '0'.repeat(paddingSize);
     const newId = `D${zeroPadding}${newIdNumber}`;
