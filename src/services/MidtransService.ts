dfadc3cc23f6921ba228c546f3c16e2fa39616c7
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-05-11 22:21:09 +0700

    axios client midtrans, cuma template aja

---
+++ src/services/MidtransService.ts
@@ -0,0 +1,9 @@
+import MidtransClient from "./clients/MidtransClient";
+
+class MidtransService {
+    async verifyPayment(code: string) {
+        // ...
+    }
+}
+
+export default new MidtransService();
