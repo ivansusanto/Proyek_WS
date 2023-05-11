506549f4cdb9263df35788d4b1da62c3fb33a704
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-05-11 22:20:29 +0700

    ngubah db

---
+++ prisma/migrations/20230511150307_hilangin/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `api_key` on the `developers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `developers` DROP COLUMN `api_key`;
