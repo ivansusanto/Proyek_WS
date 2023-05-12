7d9fd9a50b8371b61ce589ed7704cf325cb6cad0
equerlyn <feliciapangestu.fp@gmail.com>
2023-05-12 13:09:55 +0700

    Update seed.ts

--- prisma/seeder/seed.ts
+++ prisma/seeder/seed.ts
@@ -44,6 +44,121 @@ async function main() {
         },
     })
 
+    //PRODUCT
+    await prisma.products.upsert({
+        where: { product_id: 'P0001' },
+        update: {},
+        create: {
+            product_id: 'P0001',
+            name: "Adidas POD 1923",
+            description: "Sepatu adidas dengan model yang modernn edisi 1923",
+            price: 1990000,
+            stock: 28,
+            status: 1,
+            developer_id: "D0001",
+            image: "fotosepatuadidaspod1923"
+        },
+    })
+    await prisma.products.upsert({
+        where: { product_id: 'P0002' },
+        update: {},
+        create: {
+            product_id: 'P0002',
+            name: "Nike Air Force 1 Tosca",
+            description: "Sepatu nike air force 1 yang dibalut warna hijau tosca yang segar",
+            price: 2100000,
+            stock: 35,
+            status: 1,
+            developer_id: "D0001",
+            image: "fotosepatunikeairforce1"
+        },
+    })
+    await prisma.products.upsert({
+        where: { product_id: 'P0003' },
+        update: {},
+        create: {
+            product_id: 'P0003',
+            name: "Iphone 14 Pro Max",
+            description: "Iphone edisi terbaru yang ada di bumi tahun 2022 yang sangat menawan",
+            price: 23999999,
+            stock: 12,
+            status: 1,
+            developer_id: "D0002",
+            image: "fotoipong14promag"
+        },
+    })
+    await prisma.products.upsert({
+        where: { product_id: 'P0004' },
+        update: {},
+        create: {
+            product_id: 'P0004',
+            name: "Samsung S23 Mini",
+            description: "Hp mini kelas menawan keluaran samsung dari korea selatan yang lucu",
+            price: 16789000,
+            stock: 14,
+            status: 1,
+            developer_id: "D0002",
+            image: "fotohpsamsungS23mini"
+        },
+    })
+    await prisma.products.upsert({
+        where: { product_id: 'P0005' },
+        update: {},
+        create: {
+            product_id: 'P0005',
+            name: "Vivo Air Book 5",
+            description: "Laptop dengan kedap suara dan kedap air keluaran vivo",
+            price: 11200000,
+            stock: 11,
+            status: 1,
+            developer_id: "D0003",
+            image: "fotolaptopvivoairbook5"
+        },
+    })
+    await prisma.products.upsert({
+        where: { product_id: 'P0006' },
+        update: {},
+        create: {
+            product_id: 'P0006',
+            name: "ROG Strix",
+            description: "Laptop keluaran asus stuf gaming dengan kipas helikopter terbaru 2023",
+            price: 13900000,
+            stock: 9,
+            status: 1,
+            developer_id: "D0003",
+            image: "fotorogstrixstufgaming"
+        },
+    })
+    await prisma.products.upsert({
+        where: { product_id: 'P0007' },
+        update: {},
+        create: {
+            product_id: 'P0007',
+            name: "MSi Katana",
+            description: "Laptop keluaran MSi dengan spek terbagus dan terbaik MSi",
+            price: 11234567,
+            stock: 22,
+            status: 1,
+            developer_id: "D0003",
+            image: "fotolaptopmsikatana"
+        },
+    })
+    await prisma.products.upsert({
+        where: { product_id: 'P0008' },
+        update: {},
+        create: {
+            product_id: 'P0008',
+            name: "Apple Mac Book",
+            description: "Laptop teringan dan termahal yang ada di dunia keluaran Apple",
+            price: 23999999,
+            stock: 31,
+            status: 1,
+            developer_id: "D0003",
+            image: "fotoapplemacbookmahal"
+        },
+    })
+
+
     //USER
     await prisma.users.upsert({
         where: { user_id: 'U0001' },
@@ -288,119 +403,12 @@ async function main() {
         },
     })
 
-    //PRODUCT
-    await prisma.products.upsert({
-        where: { product_id: 'P0001' },
-        update: {},
-        create: {
-            product_id: 'P0001',
-            name: "Adidas POD 1923",
-            description: "Sepatu adidas dengan model yang modernn edisi 1923",
-            price: 1990000,
-            stock: 28,
-            status: 1,
-            developer_id: "D0001",
-            image: "fotosepatuadidaspod1923"
-        },
-    })
-    await prisma.products.upsert({
-        where: { product_id: 'P0002' },
-        update: {},
-        create: {
-            product_id: 'P0002',
-            name: "Nike Air Force 1 Tosca",
-            description: "Sepatu nike air force 1 yang dibalut warna hijau tosca yang segar",
-            price: 2100000,
-            stock: 35,
-            status: 1,
-            developer_id: "D0001",
-            image: "fotosepatunikeairforce1"
-        },
-    })
-    await prisma.products.upsert({
-        where: { product_id: 'P0003' },
-        update: {},
-        create: {
-            product_id: 'P0003',
-            name: "Iphone 14 Pro Max",
-            description: "Iphone edisi terbaru yang ada di bumi tahun 2022 yang sangat menawan",
-            price: 23999999,
-            stock: 12,
-            status: 1,
-            developer_id: "D0002",
-            image: "fotoipong14promag"
-        },
-    })
-    await prisma.products.upsert({
-        where: { product_id: 'P0004' },
-        update: {},
-        create: {
-            product_id: 'P0004',
-            name: "Samsung S23 Mini",
-            description: "Hp mini kelas menawan keluaran samsung dari korea selatan yang lucu",
-            price: 16789000,
-            stock: 14,
-            status: 1,
-            developer_id: "D0002",
-            image: "fotohpsamsungS23mini"
-        },
-    })
-    await prisma.products.upsert({
-        where: { product_id: 'P0005' },
-        update: {},
-        create: {
-            product_id: 'P0005',
-            name: "Vivo Air Book 5",
-            description: "Laptop dengan kedap suara dan kedap air keluaran vivo",
-            price: 11200000,
-            stock: 11,
-            status: 1,
-            developer_id: "D0003",
-            image: "fotolaptopvivoairbook5"
-        },
-    })
-    await prisma.products.upsert({
-        where: { product_id: 'P0006' },
-        update: {},
-        create: {
-            product_id: 'P0006',
-            name: "ROG Strix",
-            description: "Laptop keluaran asus stuf gaming dengan kipas helikopter terbaru 2023",
-            price: 13900000,
-            stock: 9,
-            status: 1,
-            developer_id: "D0003",
-            image: "fotorogstrixstufgaming"
-        },
-    })
-    await prisma.products.upsert({
-        where: { product_id: 'P0007' },
-        update: {},
-        create: {
-            product_id: 'P0007',
-            name: "MSi Katana",
-            description: "Laptop keluaran MSi dengan spek terbagus dan terbaik MSi",
-            price: 11234567,
-            stock: 22,
-            status: 1,
-            developer_id: "D0003",
-            image: "fotolaptopmsikatana"
-        },
-    })
-    await prisma.products.upsert({
-        where: { product_id: 'P0008' },
-        update: {},
-        create: {
-            product_id: 'P0008',
-            name: "Apple Mac Book",
-            description: "Laptop teringan dan termahal yang ada di dunia keluaran Apple",
-            price: 23999999,
-            stock: 31,
-            status: 1,
-            developer_id: "D0003",
-            image: "fotoapplemacbookmahal"
-        },
-    })
+    //ORDER
+
+
+    //ORDER_ITEM
+
+
 }
 main()
   .then(async () => {
