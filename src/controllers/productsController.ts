9fe26950982c1d1396ff74348e830596ae5183fe
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-05-13 02:05:40 +0700

    product controller & models done

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -12,26 +12,61 @@ const addProductSchema = {
     image: Joi.string().required()
 }
 
+const updateProductSchema = {
+    product_id: Joi.string().required(),
+    name: Joi.string(),
+    description: Joi.string(),
+    price: Joi.number().min(1),
+    stock: Joi.number().min(1),
+    image: Joi.string(),
+    status: Joi.number().min(0).max(1)
+}
+
 export async function addProduct(req : Request, res : Response) {
     const data = req.body;
 
     const validation = await validator(addProductSchema, data);
-
     if (validation.message) return res.status(400).json({ message: validation.message.replace("\"", "").replace("\"", "") });
     
-    const newProduct = await Product.create(req.body, await Developer.fetchByUsername(data.developer.username));
+    const newProduct = await Product.create(data, await Developer.fetchByUsername(data.developer.username));
 
-    return res.status(201).json(newProduct);
+    return res.status(201).json({
+        message: 'Success add product',
+        status: '201',
+        data: newProduct
+    });
 }
 
 export async function fetchProduct(req : Request, res : Response) {
-    
+    return res.status(200).json(await Product.get(req.body.developer.username));
 }
 
 export async function fetchProductById(req : Request, res : Response) {
-    
+    const fetchById = await Product.fetchById(req.body.developer.username, req.params.product_id);
+
+    if (fetchById.length !== 0) return res.status(200).json(fetchById);
+    return res.status(403).json({ message: 'Forbidden' });
 }
 
 export async function updateProduct(req : Request, res : Response) {
+    const data = {
+        ...req.params,
+        ...req.body
+    };
+
+    const validation = await validator(updateProductSchema, data);
+    if (validation.message) return res.status(400).json({ message: validation.message.replace("\"", "").replace("\"", "") });
     
+    delete req.body.developer;
+    if (data.price) req.body.price = parseInt(data.price);
+    if (data.stock) req.body.stock = parseInt(data.stock);
+    if (data.status) req.body.status = parseInt(data.status);
+
+    const newProduct = await Product.update(req.body, data.developer.username, data.product_id);
+    if (!newProduct) return res.status(403).json({ message: 'Forbidden' });
+    return res.status(200).json({
+        message: 'Success update product',
+        status: '200',
+        data: newProduct
+    });
 }
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -23,5 +23,73 @@ export default new (class Product {
         });
     }
 
-    
+    async get(username: string) {
+        return await prisma.products.findMany({
+            select: {
+                product_id: true,
+                name: true,
+                description: true,
+                price: true,
+                stock: true,
+                status: true,
+                image: true
+            },
+            where: {
+                developers: {
+                    username: username
+                }
+            }
+        });
+    }
+
+    async update(product: Prisma.productsUpdateInput, username: string, product_id: string) {
+        const developer_id = await prisma.developers.findFirst({
+            select: {
+                developer_id: true
+            },
+            where: {
+                username: username
+            }
+        });
+
+        const product_developer_id = await prisma.products.findFirst({
+            select: {
+                developer_id: true
+            },
+            where: {
+                product_id: product_id
+            }
+        });
+
+        if (!product_developer_id) return false;
+
+        if (developer_id && product_developer_id && developer_id.developer_id !== product_developer_id.developer_id) return false;
+
+        return await prisma.products.update({
+            where: {
+                product_id: product_id,
+            },
+            data: product
+        });
+    }
+
+    async fetchById(username: string, product_id: string) {
+        return await prisma.products.findMany({
+            select: {
+                product_id: true,
+                name: true,
+                description: true,
+                price: true,
+                stock: true,
+                status: true,
+                image: true
+            },
+            where: {
+                product_id: product_id,
+                developers: {
+                    username: username
+                }
+            }
+        });
+    }
 })();
