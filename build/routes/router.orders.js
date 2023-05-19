b26165785c9c1fc826d820d2210e9c27d5306695
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-05-20 00:14:52 +0700

    benerin build

--- build/routes/router.orders.js
+++ build/routes/router.orders.js
@@ -1,12 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
+var AuthMiddleware_1 = require("../middlewares/AuthMiddleware");
 var router = (0, express_1.Router)();
 var ordersController_1 = require("../controllers/ordersController");
+router.post('/sync', ordersController_1.syncOrderStatus);
+router.use(AuthMiddleware_1.AuthMiddleware);
 router.post('/checkout', ordersController_1.checkoutOrder);
 router.post('/payment', ordersController_1.paymentOrder);
 router.get('/', ordersController_1.fetchOrders);
 router.get('/:invoice/details', ordersController_1.fetchOrderById);
 router.get('/:customer_id', ordersController_1.fetchUserOrder);
-router.post('/sync', ordersController_1.syncOrderStatus);
 exports.default = router;
